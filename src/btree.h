155623f39a63cc4108841da73a7275d8a844a77f
Balkiska <100204853+Balkiska@users.noreply.github.com>
2024-11-16 15:56:49 +0100

    added content in btree.h | nodes and functions

--- src/btree.h
+++ src/btree.h
@@ -1,1 +1,20 @@
-//Should declare the structure of the binary tree and prototypes for functions like insert, delete, search, and balance for binary tree operations.
+#ifndef BTREE_H
+#define BTREE_H
+
+#include "row.h"
+
+// Nodes
+typedef struct BTreeNode {
+    Row* row;                   
+    struct BTreeNode* left;     
+    struct BTreeNode* right;    
+} BTreeNode;
+
+BTreeNode* search_by_id(BTreeNode* root, int id);
+BTreeNode* search_by_name(BTreeNode* root, const char* name);
+BTreeNode* search_by_email(BTreeNode* root, const char* email);
+
+void print_tree(BTreeNode* root);
+void free_tree(BTreeNode* root);
+
+#endif 
