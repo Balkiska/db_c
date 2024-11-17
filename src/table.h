d579ef066581d6ae3c0ddf0b819e7884ec11f148
Balkiska <100204853+Balkiska@users.noreply.github.com>
2024-11-17 15:12:03 +0100

    rebluid the functions and add btreenode in the structure

--- src/table.h
+++ src/table.h
@@ -1,20 +1,24 @@
 #ifndef TABLE_H
 #define TABLE_H
+#include "btree.h"
+#include "row.h"
+#include <stdbool.h>
 
-typedef struct {
-    int id;
-    char* name;
-    char* email;
-} Row;
-
+// Table structure
 typedef struct {
     Row* rows;
     int num_rows;
     int max_rows;
+    BTreeNode* index; 
 } Table;
 
-Table* new_table();
-void insert_row(Table* table, Row* row);
-void select_rows(Table* table);
+// Functions
+Table* new_table(void);
+void free_table(Table* table);
+bool insert_row(Table* table, int id, const char* name, const char* email);
+void select_all_rows(Table* table);
+void select_row_by_id(Table* table, int id);
+void select_row_by_name(Table* table, const char* name);
+void select_row_by_email(Table* table, const char* email);
 
 #endif
