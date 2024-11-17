e5506f26fd7a1ca1c9979cd721e711a27045ac3d
Balkiska <100204853+Balkiska@users.noreply.github.com>
2024-11-17 17:48:07 +0100

    added all the new functions of btree.c in btree.h

--- src/btree.h
+++ src/btree.h
@@ -13,7 +13,18 @@ typedef struct BTreeNode {
 BTreeNode* search_by_id(BTreeNode* root, int id);
 BTreeNode* search_by_name(BTreeNode* root, const char* name);
 BTreeNode* search_by_email(BTreeNode* root, const char* email);
+BTreeNode* new_node(Row* row);
+BTreeNode* insert(BTreeNode* root, Row* row);
+Row* new_row(int id, const char* name, const char* email);
+void save_tree_to_disk(BTreeNode* root);
+BTreeNode* load_tree_from_disk();
+BTreeNode* delete_by_id(BTreeNode* root, int id);
+void backup_database(BTreeNode* root, const char* filename);
+BTreeNode* restore_database(const char* filename);
 
+void free_row(Row* row);
+void free_tree(BTreeNode* root);
+void print_tree(BTreeNode* root);
 void print_tree(BTreeNode* root);
 void free_tree(BTreeNode* root);
 
